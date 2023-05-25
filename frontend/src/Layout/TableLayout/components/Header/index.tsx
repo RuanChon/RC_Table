bb47f3ca49287b2f459dad7fdfaeccbfcc75bd67
chon <724082712@qq.com>
2023-05-25 19:23:02 +0800

    fix:

--- frontend/src/Layout/TableLayout/components/Header/index.tsx
+++ frontend/src/Layout/TableLayout/components/Header/index.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   return (
     <HeaderContainer height={64}>
       <Box className="h-full pl-4 bg-red-200">
-        <IconChevronLeft /> 看到 44:00
+        <IconChevronLeft />
       </Box>
     </HeaderContainer>
   )
